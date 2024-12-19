e366db27033376d7d1967d9d237ab633737f67b8
eetann <hideharu092@gmail.com>
2024-12-19 19:05:35 +0900

    feat: Programのs/oを追加

--- packages/language-server/src/domain/model/PhpNode/ProgramStrategy.ts
+++ packages/language-server/src/domain/model/PhpNode/ProgramStrategy.ts
@@ -1,12 +1,42 @@
+import {
+	type Occurrence,
+	type SymbolInformation,
+	SymbolInformation_Kind,
+	SymbolRole,
+	SyntaxKind,
+} from "@/gen/scip_pb";
 import type { Program } from "php-parser";
-import { NodeStrategy } from "./NodeStrategy";
+import {
+	NodeStrategy,
+	createOccurrenceMultipleLine,
+	createSymbolInformation,
+} from "./NodeStrategy";
 
 export class ProgramStrategy extends NodeStrategy {
 	getSymbol(_node: Program, _parentSymbol: string): string {
 		return this.symbolCreator.filename;
 	}
+
 	getChildren(node: Program) {
 		console.log("program getChildren");
 		return node.children;
+	}
+
+	createSymbolInformation(node: Program): SymbolInformation[] {
+		return [
+			createSymbolInformation({
+				symbol: node.symbol,
+				kind: SymbolInformation_Kind.Namespace,
+			}),
+		];
+	}
+
+	createOccurrenceMultipleLine(node: Program): Occurrence[] {
+		return [
+			createOccurrenceMultipleLine(node.symbol, node, {
+				syntaxKind: SyntaxKind.IdentifierFunctionDefinition,
+				symbolRoles: SymbolRole.Definition,
+			}),
+		];
 	}
 }
