e05b44c3ba26dd2df3ab56e20ef6fb1d536321ca
eetann <hideharu092@gmail.com>
2024-12-20 16:39:31 +0900

    feat: ワークスペースのパス取得を追加

--- packages/language-server/src/presentation/LanguageServicePluginInstanceCreator.ts
+++ packages/language-server/src/presentation/LanguageServicePluginInstanceCreator.ts
@@ -1,5 +1,6 @@
+import type { Document } from "@/gen/scip_pb";
+import { CreateIndexUseCase } from "@/usecase/createIndex/CreateIndexUseCase";
 import { ProvideCompletionItemsUseCase } from "@/usecase/provideCompletionItems/ProvideCompletionItemsUseCase";
-import { Indexer } from "@/usecase/shared/indexer/Indexer";
 import {
 	type Connection,
 	type LanguageServiceContext,
@@ -7,10 +8,11 @@ import {
 	MessageType,
 	ShowMessageNotification,
 } from "@volar/language-server";
+import { URI } from "vscode-uri";
 
 export class LanguageServicePluginInstanceCreator {
 	private initializationStarted = false;
-	private indexer = new Indexer();
+	private index: Document;
 	constructor(private connection: Connection) {}
 
 	// Use arrow function to keep `this` in the defined scope
@@ -20,7 +22,7 @@ export class LanguageServicePluginInstanceCreator {
 		return {
 			provideCompletionItems: async (...args) =>
 				await this.runWithInitialization(async () =>
-					new ProvideCompletionItemsUseCase(this.indexer).execute(...args),
+					new ProvideCompletionItemsUseCase(this.index).execute(...args),
 				),
 		};
 	};
@@ -36,13 +38,25 @@ export class LanguageServicePluginInstanceCreator {
 		this.initializationStarted = true;
 		const progress = await this.connection.window.createWorkDoneProgress();
 		progress.begin("initializing...");
+		this.connection.workspace.getWorkspaceFolders();
+		const workspaceFolders = await this.getWorkspaceFolders();
+		console.log(workspaceFolders);
 		// ここでindexする
-		this.indexer.setIndex();
 		await new Promise((res) => setTimeout(res, 1000));
 		progress.done();
 		this.connection.sendNotification(ShowMessageNotification.type, {
 			type: MessageType.Info,
 			message: "Laravel Language Server initialized.",
 		});
+	}
+
+	async getWorkspaceFolders() {
+		const workspaceFolders: string[] = [];
+		const folders =
+			(await this.connection.workspace.getWorkspaceFolders()) ?? [];
+		for (const folder of folders) {
+			workspaceFolders.push(URI.parse(folder.uri).fsPath);
+		}
+		return workspaceFolders;
 	}
 }
